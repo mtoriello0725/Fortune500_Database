2a95b3354edc95563e947e9cae3344d635ab79d8
Mike Toriello <34101012+mtoriello0725@users.noreply.github.com>
2018-11-03 14:03:48 -0400

    Condensing files

---
+++ MySQL_files/insert_jobs.sql
@@ -0,0 +1,10 @@
+use fortune500_db;
+
+-- Create Table
+CREATE TABLE jobs (job_age VARCHAR(15), company VARCHAR(255), job_desc VARCHAR(255), jcid VARCHAR(20), jkid VARCHAR(20), job_no INT, job_loc VARCHAR(255), query_date DATE, query_time VARCHAR(8), job_title VARCHAR(255));
+
+-- job_desc would not insert into mysql without ALTER TABLE jobs CONVERT TO CHARACTER SET utf8
+ALTER TABLE jobs CONVERT TO CHARACTER SET utf8;
+
+insert into fortune500_db.jobs (job_age, company, job_desc, jcid, jkid, job_no, job_loc, query_date, query_time, job_title)
+select * from indeed.jobs;
